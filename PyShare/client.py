9860c14d2d848265b7c6ace21e708dffb65cac32
Varun Malhotra <varun2902@gmail.com>
2013-05-09 19:23:01 +0530

    Improved GUI, Client-to-client chat

--- PyShare/client.py
+++ PyShare/client.py
@@ -1,7 +1,7 @@
 #install_twisted_rector must be called before importing the reactor
 
 import getpass
-import platform
+import platform, subprocess
 from threading import Thread
 import os
 import datetime
@@ -9,6 +9,9 @@ import sys
 import sys
 import time
 import logging
+
+from functools import partial
+
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
@@ -51,6 +54,7 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.popup import Popup
+from kivy.animation import Animation
 
 # A simple kivy App, with a textbox to enter messages, and
 # a large label to display all the messages received from
@@ -60,66 +64,171 @@ class TwistedClientApp(App):
 	user_os = None
 	user = None
 	labelmsg = ""
+	chat_online_details = ""
+	haha = []
+	y=0
+	notify_events = []
+	available_users = []
 	def build(self):
-		root = self.setup_gui()
+		root = self.pro_info()
         #self.connect_to_server()
 		self.start_thread()
 		return root
-	
-	def setup_gui(self):
-        #self.textbox = TextInput(size_hint_y=.1, multiline=False)
-        #self.textbox.bind(on_text_validate=self.send_message)
+	def pro_info(self):
+		self.layout = FloatLayout(size=(800,800))
+		self.Mainlabel = Label(text="SharePy",color=(0.6,0.7,0.2,1),font_size="65sp",pos=(280,450),size_hint=(.3,.3))
+		self.layout.add_widget(self.Mainlabel)
+		self.cont_but = Button(text="Cont...",background_color=(0.2,0.3,0.88,1),pos=(700,30),size_hint=(.12,.1))
+		self.layout.add_widget(self.cont_but)
+		self.INFO = Label(size_hint=(.3,.3),pos=(230,300),color=(0.6,0.3,0.1,1),font_size="21dp")
+		self.INFO2 = Label(size_hint=(.3,.3),pos=(230,150),color=(0.3,0.3,0.7,1),font_size="21dp")
+		self.INFO.text = "SharePy is a project based on File Sharing.\nIts a project developed in Kivy using Python.\n\nBasic Features include:\n "
+		self.INFO2.text = self.INFO2.text + "\n-> Zero Configuration\n-> File Transfering\n-> File Syncing\n-> File Searching\n-> Notification Broadcasting on File Events\n-> ChatBot for communication between Clients\n-> File Listing"
+		self.INFO3 = Label(text="Members:\nVarun Malhotra\nMayank Bhola\nHarsh Bhatia",color=(0.7,0.1,0.1,1),pos=(150,40),size_hint=(0.2,0.2),font_size="21dp")
 		
-		self.label = TextInput(hint_text='Connecting...\n',size_hint=(.6,.5),pos=(280,120),readonly=True,focus=True)
-		self.dir_loc = TextInput(hint_text='Your root directory is: ',size_hint=(.6,.07),pos=(150,430),readonly=True,background_color=(192,192,192,0.3),foreground_color=(255,255,255,1))
-		self.button =Button(text="Start Client",size_hint=(.2,.2),pos=(50,100))
-	
-		self.ctexthost = TextInput(hint_text="Enter host",size_hint=(.2,.1),pos=(50,350),multiline=False,background_color=(0,0,255,0.3),foreground_color=(255,255,255,1))
-		self.ctextport = TextInput(hint_text="Enter port",size_hint=(.2,.1),pos=(50,250),multiline=False,background_color=(0,0,255,.3),foreground_color=(255,255,255,1))
-
-		self.button1 = ToggleButton(text="Search a File",size_hint=(.2,.2),pos=(10,500),group="file_share",font_size='18sp')
-		self.button2 = ToggleButton(text="Get a File",size_hint=(.2,.2),pos=(210,500),group="file_share",font_size="18sp")
-		self.button3 = Button(text="Search a File",size_hint=(.2,.2),pos=(150,480),background_color=(0,255,255,0.7),font_size="18sp")
-		self.button4 = Button(text="List Directory",size_hint=(.2,.2),pos=(470,480),background_color=(0,255,0,0.5),font_size="18sp")
-		self.button4.bind(on_press=self.opentextinput)
+		self.layout.add_widget(self.INFO)
+		self.layout.add_widget(self.INFO2)
+		self.layout.add_widget(self.INFO3)
+		self.anim_info2 = Animation(x=80,y=150, opacity=0.4, d=0.4,t ='in_quad') +\
+			Animation(x=230,y=150, opacity=1, d=0.5)
+		self.anim_info2.start(self.INFO2)
+		self.anim_info3 = Animation(x=80,y=20, opacity=0.4, d=0.6,t ='in_quad') +\
+			Animation(x=150,y=20, opacity=1, d=0.8)
+		self.anim_info3.start(self.INFO3)
+		self.cont_but.bind(on_press=self.setup_gui)
 		
-		self.button.bind(on_press=self.connect_to_server)	
-		self.layout = FloatLayout(size=(600,600))
-		
-		self.layout.add_widget(self.label)
-		self.layout.add_widget(self.dir_loc)
-		self.layout.add_widget(self.button)
-		#self.layout.add_widget(self.button1)
-		#self.layout.add_widget(self.button2)
-		self.layout.add_widget(self.button3)
-		self.layout.add_widget(self.button4)
-		self.layout.add_widget(self.ctexthost)
-		self.layout.add_widget(self.ctextport)
+		return self.layout
+	def setup_gui(self,object):
 		global user 
 		user = getpass.getuser()
 		global user_os 
 		user_os = platform.system()
-		root_dirname = "Hellosnp"
-		if not os.path.exists(root_dirname):
-			os.makedirs(root_dirname)
-		os.chdir(root_dirname)
 		
-		self.dir_loc.text = os.getcwd()
-		return self.layout
+		self.Mainlabel = Label(text="SharePy",color=(0.6,0.7,0.2,1),font_size="65sp",pos=(280,450),size_hint=(.3,.3))
+		
+		self.label = TextInput(hint_text='Connecting...\n',size_hint=(.68,.5),pos=(20,120),readonly=True,focus=True)
+		self.dir_loc = TextInput(hint_text='Your root directory is: ',size_hint=(.68,.07),pos=(20,430),readonly=True,background_color=(192,192,192,0.3),foreground_color=(255,255,255,1))
+		self.button =Button(text="Start Client",size_hint=(.2,.2),background_color=(0.7,0.2,0.3,1),pos=(50,100))
+	
+		self.ctexthost = TextInput(text="localhost",hint_text="Enter host",size_hint=(.2,.1),pos=(-150,350),multiline=False,background_color=(0.4,0.5,1,0.9),foreground_color=(255,255,255,1))
+		self.ctextport = TextInput(text="8000",hint_text="Enter port",size_hint=(.2,.1),pos=(-150,250),multiline=False,background_color=(0.4,0.5,1,.9),foreground_color=(255,255,255,1))
 
+		self.button1 = ToggleButton(text="Search a File",size_hint=(.2,.2),pos=(10,500),group="file_share",font_size='18sp')
+		self.button2 = ToggleButton(text="Get a File",size_hint=(.2,.2),pos=(210,500),group="file_share",font_size="18sp")
+		self.button3 = Button(text="Search a File",size_hint=(.17,.15),pos=(20,520),background_color=(0.5,0.7,1,1),font_size="18sp")
+		self.button4 = Button(text="List Directory",size_hint=(.17,.15),pos=(150,520),background_color=(0.4,1,0.3,1),font_size="18sp")
+		self.button4.bind(on_press=self.opentextinput)
+		
+		self.closeclient = Button(text="Close Client",pos=(425,520),size_hint=(0.17,0.15),background_color=(0.8,0.3,0.1,1),font_size="18sp")
+		self.closeclient.bind(on_press=self.CloseClient)
+		
+		self.notify_ground = TextInput(text="I notify events",pos=(600,0),background_color=(0.7,0.5,0.9,1),size_hint=(.3,1),readonly=True,scroll_y=10)
+		self.chat_ground = TextInput(text="CHAT BOX",pos=(600,0),background_color=(0.7,0.5,0.9,1),size_hint=(.3,1),readonly=True,scroll_y=10)
+		
+		self.chatbox = Button(text="Chat Box",pos=(563,520),size_hint=(0.17,0.15),background_color=(0.3,0.3,0.7,1),font_size="18sp")
+		self.chatbox.bind(on_press=self.chathistory)
+		
+		self.reponame = TextInput(text="Hellosnp",hint_text="Enter RepoName",pos=(200,300),size_hint=(0.5,0.1),multiline=False,focus=True)
+		
+		self.button.bind(on_press=self.connect_to_server)
+		self.label_info = TextInput(text="Hi i'm a alebe;",pos=(0,610),background_color=(0.7,0,0,1),size_hint=(1,.2),readonly=True)
+		self.notify_but = Button(text="Recent events",size_hint=(.17,.15),pos=(290,520),background_color=(0.8,0.2,0.3,1),font_size="18sp")
+		self.notify_but.bind(on_press=self.recent_act)
+		
+		self.cancel_event = Button(text="Close",size_hint=(0.27,0.06),pos=(600,0),background_color=(255,0,0,0.6))
+		self.cancel_chat = Button(text="Close",size_hint=(0.27,0.06),pos=(600,0),background_color=(255,0,0,0.6))
+		self.repolabel = Label(text="Create your own Repository/New Folder by providing the name below",pos=(210,320),size_hint=(0.5,0.5),color=(0.6,0.3,0.1,1),font_size="20dp")
+		#self.layout = FloatLayout(size=(800,800))
+		
+		self.update = Button(text="Update",background_color=(0.3,0.3,0.9,1),size_hint=(.25,.1),pos=(600,550))
+		
+		self.setup_load()
+		
+		
+		return self.layout
+	def setup_load(self):
+		
+		self.layout.clear_widgets()
+		self.layout.add_widget(self.button)
+		self.layout.add_widget(self.Mainlabel)
+		self.layout.add_widget(self.ctexthost)
+		self.layout.add_widget(self.ctextport)
+		
+		self.anim_host = Animation(x=280,y=550, opacity=0.4, d=0.2,t ='in_quad') +\
+			Animation(x=280,y=450, opacity=1, d=0.2)
+		self.anim_host.start(self.Mainlabel)
+		
+		self.anim_host = Animation(x=0,y=350, opacity=0.4, d=0.5,t ='in_quad') +\
+			Animation(x=50,y=350, opacity=1, d=0.6)
+		self.anim_host.start(self.ctexthost)
+		
+		self.anim_port = Animation(x=0,y=250, opacity=0.4, d=0.5,t ='in_quad') +\
+			Animation(x=50,y=250, opacity=1, d=0.6)
+		self.anim_port.start(self.ctextport)
+		
+		self.anim_startbut = Animation(x=0,y=100, opacity=0.4, d=0.5,t ='in_quad') +\
+			Animation(x=50,y=100, opacity=1, d=0.6)
+		self.anim_startbut.start(self.button)
+	def setup_utilities(self):
+		self.layout.clear_widgets()
+		#self.repolabel = Label(text="Create your own Repository/New Folder by providing the name below",pos=(200,500))
+		#self.repolabel.text = "Create your own Repository/New Folder by providing the name below"
+		self.layout.add_widget(self.repolabel)
+		self.layout.add_widget(self.reponame)
+		self.reponame.bind(on_text_validate=self.start_utilities)
+		return self.layout
+	def start_utilities(self, *args):
+			root_dirname = self.reponame.text
+			if not os.path.exists(root_dirname):
+				os.makedirs(root_dirname)
+				#output = subprocess.Popen(["git","init"],shell=True,stdout=subprocess.PIPE).communicate()[0]
+			os.chdir(root_dirname)
+			self.dir_loc.text = os.getcwd()
+			self.layout.clear_widgets()
+			self.layout.add_widget(self.button3)
+			self.layout.add_widget(self.button4)
+			self.layout.add_widget(self.closeclient)
+			self.layout.add_widget(self.notify_but)
+			self.layout.add_widget(self.chatbox)
+			self.layout.add_widget(self.label)
+			self.layout.add_widget(self.dir_loc)
+			self.layout.add_widget(self.label_info)
+			
+			self.anim_host = Animation(x=20,y=520, opacity=0.4, d=0.5,t ='in_quad') +\
+				Animation(x=20,y=480, opacity=1, d=0.6)
+			self.anim_host.start(self.button3)
+		
+			self.anim_port = Animation(x=155,y=520, opacity=0.1, d=0.5,t ='in_quad') +\
+				Animation(x=155,y=480, opacity=1, d=0.6)
+			self.anim_port.start(self.button4)
+		
+			self.anim_button = Animation(x=290,y=520, opacity=0.1, d=0.5,t ='in_quad') +\
+				Animation(x=290,y=480, opacity=1, d=0.6)
+			self.anim_button.start(self.notify_but)
+			
+			self.anim_close = Animation(x=425,y=520, opacity=0.1, d=0.5,t ='in_quad') +\
+				Animation(x=425,y=480, opacity=1, d=0.6)
+			self.anim_close.start(self.closeclient)
+			
+			self.anim_chat = Animation(x=563,y=520, opacity=0.1, d=0.5,t ='in_quad') +\
+				Animation(x=563,y=480, opacity=1, d=0.6)
+			self.anim_chat.start(self.chatbox)
+			
+			return self.layout
 	def connect_to_server(self,*args):
 	
 		if((self.ctextport.text).isdigit()):
-			reactor.connectTCP(str(self.ctexthost.text), int(self.ctextport.text), EchoFactory(self))
+			self.conn = reactor.connectTCP(str(self.ctexthost.text), int(self.ctextport.text), EchoFactory(self))
 			#client_info = "0$#" + 
 			#self.connection.write()
 			self.label.text = "Connecting...\n"
 		else:
 			
-			self.label.text  = "Connecting...\nPlease enter valid port" 	
+			self.label.text  = "Not Connected...\nPlease enter valid port" 	
 	def on_connection(self, connection):
-		self.print_message("connected succesfully!")
+		self.print_message("Connected Succesfully!")
 		self.connection = connection
+		self.setup_utilities()
 
 	def send_message(self,connection):
 		msg = self.textbox.text
@@ -130,11 +239,15 @@ class TwistedClientApp(App):
 
 	def print_message(self, msg):
 		copr = msg.split("$#")[0]
-		print msg
+		
+		#print msg
 		if copr.isdigit():
 			if (int(copr)==2):
 				self.label.text = msg.split("$#")[1] + "\n"
-				print msg.split("$#")[1]
+				#print msg.split("$#")[1]
+			elif (int(copr)==8):
+				self.sendarea.text = self.sendarea.text + msg.split("8$#")[1] + "\n"
+				print "recveived msg - " + (msg.split("8$#")[1]).split("##")[1] + " from ip add" + (msg.split("8$#")[1]).split("##")[0]
 			elif (int(copr)==4):
 				msg = msg.split("$#")[1]
 				g = msg.split(": <")[1]
@@ -151,8 +264,33 @@ class TwistedClientApp(App):
 					f = g.split("src_path=")[1]
 					f1 = f.split(">")[0]
 					msg = "A file at\n" + f1 + "\n is " + o
-				self.popup = Popup(title='Notification      Click outside to exit',content=Label(text=str(msg)),size_hint=(.8, .4))
-				self.popup.open()
+				#self.popup = Popup(title='Notification      Click outside to exit',content=Label(text=str(msg)),size_hint=(.8, .4))
+				#self.popup.open()
+				self.layout.remove_widget(self.label_info)
+				self.layout.add_widget(self.label_info)
+				self.label_info.text = str(msg)
+				self.anim = Animation(x=0,y=550, opacity=0, d=1, t='in_back') +\
+					Animation(x=0,y=480, d=1, opacity=0.8) +\
+					Animation(x=0,y=480, d=4,opacity=1) +\
+					Animation(x=0,y=650, opacity=0, d=2)
+				self.anim.start(self.label_info)
+				self.notify_events.append(str(msg))
+				#call function recent_act
+			elif (int(copr)==5):
+				msg = msg.split("$#")[1]
+				#self.available_users.append(str(msg))
+				msg = msg.split(',')
+				self.chat_ground.text = " "
+				self.haha =  [0]*30
+				self.y = 0
+				#print len(msg)
+				for on in range (0,len(msg)-1):
+					self.haha[on] = Button(text=str(msg[on]),background_color=(0,1,0,1),pos=(600,515-self.y),size_hint=(0.25,0.07))
+					self.layout.add_widget(self.haha[on])
+					self.haha[on].bind(on_press= partial(self.open_chat_for,msg[on]))
+					self.y=self.y+45
+				self.layout.add_widget(self.update)	
+			return self.layout	
 		else:
 			self.label.text = msg + "\n"
 	def opentextinput(self, *args):
@@ -162,10 +300,42 @@ class TwistedClientApp(App):
 		self.closebutton.bind(on_press=self.destroy)
 		self.layout.add_widget(self.textbox)
 		self.layout.add_widget(self.closebutton)
-
+	
+		
 	def destroy(self, *args):
 		self.layout.remove_widget(self.closebutton)
-		self.layout.remove_widget(self.textbox)	
+		self.layout.remove_widget(self.textbox)
+
+	def CloseClient(self, *args):
+		self.conn.disconnect()
+		self.layout.clear_widgets()
+		#os.chdir(cd..)
+		self.setup_load()
+		path = os.path.dirname(os.getcwd())
+		os.chdir(path)
+		#output = subprocess.Popen(["cd.."],stdout=subprocess.PIPE).communicate()[0]
+		print os.getcwd()
+		return self.layout
+		
+	def chathistory(self, *args):
+		self.layout.remove_widget(self.chatbox)
+		self.layout.clear_widgets()
+		
+		
+		self.sendarea = Label(text=" ",size_hint=(.2,.2),pos=(80,400),font_size="19dp",color=(0.7,0.5,0.6,1))
+		self.layout.add_widget(self.sendarea)
+		
+		self.update.bind(on_press=self.cancel_chat_box)
+		self.layout.add_widget(self.chat_ground)
+		self.backbutton = Button(text="Back <-",background_color=(1,0.1,0.1,1),size_hint=(.11,.1),pos=(30,500),font_size="20dp")
+		self.layout.add_widget(self.backbutton)
+		self.backbutton.bind(on_press=self.start_utilities)
+		#self.layout.add_widget(self.cancel_chat)
+		#self.cancel_chat.bind(on_press=self.cancel_chat_box)
+		self.chat_online_details = "5$#" + user + "@" + user_os + ": " + "Ready to chat"
+		#print self.chat_online_details
+		self.connection.write(str(self.chat_online_details))
+		#return self.layout
 
 	def background_stuff(self):	
 		while True:
@@ -194,6 +364,44 @@ class TwistedClientApp(App):
 		return self.layout
 	def dismiss_popup(instance):
 		return False
+
+	def recent_act(self, textevent):
+		self.layout.remove_widget(self.notify_but)
+		self.layout.add_widget(self.notify_ground)
+		self.anim_ground = Animation(x=700,y=0, opacity=0.3, d=0.2,t ='in_quad') +\
+			Animation(x=600,y=0, opacity=1, d=0.2)
+		self.anim_ground.start(self.notify_ground)
+		
+		
+		self.layout.add_widget(self.cancel_event)
+		self.cancel_event.bind(on_press=self.cancel_event_window)
+		self.notify_ground.text = ""
+		for l in self.notify_events:
+			self.notify_ground.text = self.notify_ground.text + str(l) + '\n'
+		return self.layout
+	def cancel_event_window(self,*args):
+		self.layout.remove_widget(self.notify_ground)
+		self.layout.remove_widget(self.cancel_event)
+		self.layout.add_widget(self.notify_but)
+		return self.layout
+		
+	def cancel_chat_box(self,args):
+		
+		self.layout.clear_widgets()
+		self.chathistory()
+		return self.layout
+		
+	def open_chat_for(self,ipadd,args):
+		self.chat_lid = TextInput(hint_text="Send msg",pos=(100,50),size_hint=(.6,.1),multiline=False,focus=True)
+		self.layout.add_widget(self.chat_lid)
+		self.chat_lid.bind(on_text_validate=partial(self.send_to_ip,ipadd))
+	def send_to_ip(self,ipadd,args):	
+		
+		print "send msg - '" + self.chat_lid.text +"' to ip address - "+ipadd
+		self.connection.write("8$#"+str(ipadd)+"##"+str(self.chat_lid.text))
+		self.chat_lid.text = ""
+		self.chat_lid.focus = True
+		
 class ChangeHandler(FileSystemEventHandler):
 	def __init__(self,obj):
 		self.obj = obj
@@ -204,7 +412,7 @@ class ChangeHandler(FileSystemEventHandler):
 			global textevent
 			textevent = event 
 			self.obj.notify(textevent)
-		if self.getext(event.src_path) == ".txt":
+		if ( self.getext(event.src_path) == ".txt" or self.getext(event.src_path) == ".py"):
 			print "e=",event
 			global textevent
 			textevent = event
@@ -213,6 +421,11 @@ class ChangeHandler(FileSystemEventHandler):
 	def getext(self,filename):
 		return os.path.splitext(filename)[-1].lower()
 
+class SendMessage(protocol.Protocol):
+	def __init__(self,obj):
+		self.obj = obj
+		self.transport.write("8$#Client.py")
+		
 if __name__ == '__main__':
 	t =  TwistedClientApp()
 	t.run()
--- PyShare/server.py
+++ PyShare/server.py
@@ -23,6 +23,7 @@ class EchoProtocol(protocol.Protocol):
         self.client_info = self.temp.split(", ")[1]
         self.client_info = self.client_info.split("'")[1]
         self.factory.delClient( self,self.client_info )
+
     def dataReceived(self, data):
         proc = data.split("$#")[0]
         if (int(proc)==2):
@@ -31,28 +32,61 @@ class EchoProtocol(protocol.Protocol):
 			    self.transport.write(response)
         elif (int(proc)==4):
             response = self.factory.sendAll(self.factory.app.handle_message(data))
+        elif (int(proc)==5):
+            response = self.factory.app.handle_message(data)
+            if response:
+                self.transport.write(response)			
+        elif (int(proc)==8):
+            response = self.factory.sendTo(self,data)
         
-
 class EchoFactory(protocol.Factory):
     protocol = EchoProtocol
     numProtocols = 0
     clients = []
     clientss = []
+    
     def __init__(self, app):
         self.app = app
         #self.obj = obj
     def addClient(self, obj,newclient):
         self.clients.append( newclient )
         self.clientss.append(obj)
+        #print self.clientss
         self.app.client_conn(self.clients)
     def delClient(self, obj,client):
         self.clients.remove( client )
         self.clientss.remove(obj)
+        #print self.clientss
         self.app.client_conn(self.clients)
     def sendAll(self, message):
-        print self.clients
+        #print self.clients
         for proto in self.clientss:
                 proto.transport.write( message )
+    def sendTo(self,obj,data):
+        i = j = 0
+        pos = 0
+        print data
+        data = data.replace(" ","")
+        d1 = data.split("8$#")[1]
+        print d1
+        d2 = d1.split("##")[0]
+        print d2
+        forward_msg_to = []
+        d3 = d1.split("##")[1]
+        for scan_ip in self.clients:
+            print scan_ip
+            if (d2==scan_ip):
+                pos = i
+            i = i + 1
+        print pos
+        for search in self.clientss:
+            if (j==pos):
+                forward_msg_to.append(search)
+            j = j + 1
+        print search
+        for send_to in forward_msg_to:
+            send_to.transport.write(data)
+		
 
 
 from kivy.app import App
@@ -64,35 +98,31 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 import subprocess
-import os
+import os, platform
 
 class TwistedServerApp(App):
+    online_c = 0
     def build(self):
 	root = self.setup_gui()
-        #self.label = Label(text="server started\n")
-        #reactor.listenTCP(8000, EchoFactory(self))
-        #self.start_server()
+        
 	return root
 
     def setup_gui(self):
-	#self.layout = FloatLayout(size=(300,300))
+	
 	self.layout =  FloatLayout(size=(300, 300))
-
+	self.online = Button(text="None",size_hint=(.06,.06),pos=(10,100))
 	self.textbox = TextInput(size_hint_y=.1,pos=(0,0))
-    	self.textport = TextInput(hint_text="Enter port no",size_hint=(.18,.1),pos=(100,200),focus=True,multiline=False,background_color=(0,255,255,0.7),foreground_color=(255,255,255,1),font_size=17)	
-	#self.layout =  FloatLayout(size=(200, 200))
-	#self.textport.bind(text=on_text)
-	self.button = Button(text='Start Server By clicking on it',size_hint=(.3,.25),pos=(280,200))
+	self.textport = TextInput(hint_text="Enter port no",size_hint=(.18,.1),pos=(160,200),focus=True,multiline=False,background_color=(0,255,255,0.7),foreground_color=(255,255,255,1),font_size=17)	
+	
+	self.button = Button(text='Start Server By clicking Button',size_hint=(.27,.2),background_color=(0.5,0.7,1,1),pos=(360,180))
 	#self.button = Button(text='Start Server',size_hint=(.5,.1))
 	self.button.bind(on_press=self.start_server)
 	
-	#self.layout =  FloatLayout(size=(400, 400))
-	self.label = Label(text="Start Server by clicking Button",size_hint=(.5,.5),pos=(190,300))
-	#self.layout = BoxLayout(orientation='vertical')
-	#self.textbox = TextInput(size_hint_y=.1,multiline=False)
-	#self.layout =  FloatLayout(size=(300, 300))
-	self.avail = Label(text="Available Clients",size_hint=(.2,.2),pos=(10,480),readonly=True,background_color=(255,255,0,0.6))
-	self.cl_conn = TextInput(text="NONE ",size_hint=(.7,.1),pos=(160,500))
+	
+	self.label = Label(text="Server is currently down",font_size="19dp",size_hint=(.5,.5),color=(0.9,0.1,0.1,1),pos=(190,300))
+	
+	self.avail = Label(text="Available Clients",size_hint=(.2,.2),color=(0.1,0.9,0.1,1),font_size="19dp",pos=(10,480),readonly=True,background_color=(255,255,0,0.6))
+	self.cl_conn = TextInput(text="NONE ",size_hint=(.7,.1),foreground_color=(0.1,0.1,0.9,1),pos=(160,500))
 	
 	self.layout.add_widget(self.cl_conn)
 	self.layout.add_widget(self.avail)
@@ -105,12 +135,10 @@ class TwistedServerApp(App):
     def start_server(self,touch):
 	self.label.text = 'Server Started'
 	self.button.text = 'Server Started'
-	#self.layout = AnchorLayout(anchor_x='right',anchor_y='top')
-	#self.layout.add_widget(self.label)
-	#self.label.text = self.textport.text
+	
 	if((self.textport.text).isdigit()):
 		reactor.listenTCP(int(self.textport.text), EchoFactory(self))
-		#print c + "lol" + r
+		
 		self.label.text = 'Server Started'
 	else:
 		self.label.text = 'Server Not Responding :( Enter valid port'
@@ -120,13 +148,15 @@ class TwistedServerApp(App):
 	self.print_message("New Client")
     
     def handle_message(self, msg):
+        
         opr = msg.split("$#")[0]
         msg = msg.split("$#")[1]
         print msg
         #self.label.text  = "received:  %s\n" % msg
         self.label.text  = "received: Event\n"
         splitmsg = msg.split(":")[0]
-        curos = splitmsg.split("@")[1]
+        #curos = splitmsg.split("@")[1]
+        curos = platform.system()
         if (opr.isdigit()):
             if (int(opr)==2):
                 self.label.text  = "received:  %s\n" % msg
@@ -135,7 +165,7 @@ class TwistedServerApp(App):
         if (int(opr)==2):
             if (curos=='Linux'):
                 output = subprocess.Popen(["find","/home/","-name",msg+"*"],stdout=subprocess.PIPE).communicate()[0]
-                msg = opr + "$#" + output
+                msg = opr + "$#"  + output
             if (curos=='Windows'):
                 #os.chdir('C:\\Users\\Varun Malhotra\\Desktop\\')
                 
@@ -144,15 +174,34 @@ class TwistedServerApp(App):
                 #print msg
         if (int(opr)==4):
             msg = opr + "$#" + msg
+        if (int(opr)==5):
+            msg = opr + "$#" + str(self.cl_conn.text)
+        if (int(opr)==8):
+            msg = opr + "$#" + "lol"
+            self.label.text  = "received:  %s\n" % msg
         return msg
 
     def client_conn(self,conn_info):
         self.cl_conn.text = ""
+        #y=20
+        #on=0
+        
         for t in conn_info:
-		    self.cl_conn.text = self.cl_conn.text + str(t) + " , "
+            #self.onlines = self.onlines + on
+            #self.onlines = Button(text="yes",size_hint=(.06,.06),pos=(10,400-y))
+            #self.layout.add_widget(self.onlines)
+            self.cl_conn.text = self.cl_conn.text + str(t) + " , "
+            #y=y+40
+            #on=on+1
+        #global online_c
+        #online_c = on
         if (self.cl_conn.text==""):
-		    self.cl_conn.text = "None Available"
+            self.cl_conn.text = "None Available"
+        
+        return self.layout
+		
 
 if __name__ == '__main__':
+    
     TwistedServerApp().run()
 
